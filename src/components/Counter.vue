dd745164f03ed7c66ef3c7a8f2f0850e7277d6d9
Dice <tetradice@gmail.com>
2022-01-16 10:13:43 +0900

    Counter分割、機能拡張

---
+++ src/components/Counter.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0)
+</script>
+
+<template>
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  <button type="button" @click="count--">1減らす</button>
+  <button type="button" @click="count *= 2">2倍にする</button>
+  <button type="button" @click="count = 0">リセット</button>
+  <br />
+</template>
